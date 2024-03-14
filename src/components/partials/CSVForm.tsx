109ec1fc19bcc2027e2f7030e0b8ef2eda7091f2
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2024-03-14 09:11:15 +0100

    feat: add card component to form

--- src/components/partials/CSVForm.tsx
+++ src/components/partials/CSVForm.tsx
@@ -12,6 +12,7 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const formSchema = z.object({
   file: z.instanceof(File),
@@ -31,35 +32,42 @@ export function CSVForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>File</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  placeholder="file.csv"
-                  type="file"
-                  accept=".csv"
-                  value={undefined}
-                  onChange={(e) => {
-                    field.onChange(e.target.files?.[0]);
-                  }}
-                />
-              </FormControl>
-              <FormDescription>
-                Upload a CSV file with times in a single column
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Upload a CSV file</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>File</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="file.csv"
+                      type="file"
+                      accept=".csv"
+                      value={undefined}
+                      onChange={(e) => {
+                        field.onChange(e.target.files?.[0]);
+                      }}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Upload a CSV file with times in a single column
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
