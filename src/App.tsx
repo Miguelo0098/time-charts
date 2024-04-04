5793627e8eeeca0a51e2b4b35aeafea9366fd5b4
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2024-04-04 15:16:10 +0200

    feat: add dark mode toggle support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { ThemeProvider } from "./components/contexts/ThemeProvider";
 import { CSVForm } from "./components/partials/CSVForm";
+import { ModeToggle } from "./components/partials/ModeToggle";
 import { TimeRecordChart } from "./components/partials/TimeRecordChart";
 import { useTimeRecordsStore } from "./hooks/useTimeRecordsStore";
 
@@ -6,18 +8,24 @@ function App() {
   const { timeRecords, interval } = useTimeRecordsStore();
 
   return (
-    <main className="w-full p-4 flex flex-col items-center gap-y-4">
-      <h1 className="text-4xl font-bold">Time records</h1>
-      <p>Check time distribution between fastest and slowest records</p>
-      <section className="w-full max-w-[720px]">
-        <CSVForm />
-      </section>
-      {timeRecords && (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="w-full p-4 flex flex-col items-center gap-y-4">
+        <header className="w-full flex justify-between items-center">
+          <h1 className="text-4xl font-bold">Time records</h1>
+          <ModeToggle />
+        </header>
+
+        <p>Check time distribution between fastest and slowest records</p>
         <section className="w-full max-w-[720px]">
-          <TimeRecordChart timeRecords={timeRecords} interval={interval} />
+          <CSVForm />
         </section>
-      )}
-    </main>
+        {timeRecords && (
+          <section className="w-full max-w-[720px]">
+            <TimeRecordChart timeRecords={timeRecords} interval={interval} />
+          </section>
+        )}
+      </main>
+    </ThemeProvider>
   );
 }
 
